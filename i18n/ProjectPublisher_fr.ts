f79c2be9291cec8dc0c44581dbcebab83a594bcd
Nicolas AUBIN <aubinnic@gmail.com>
2022-10-03 10:23:02 +0200

    Initial commit

---
+++ i18n/ProjectPublisher_fr.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>ProjectPublisher</name>
+    <message>
+        <location filename="../project_publisher.py" line="202"/>
+        <source>&amp;Project Publisher for QWC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../project_publisher.py" line="171"/>
+        <source>Project publisher</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ProjectPublisherDockWidget</name>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="138"/>
+        <source>Select an auth id</source>
+        <translation>Selectionner au AuthId</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="142"/>
+        <source>QWC Project publisher service URL not defined</source>
+        <translation>L&apos;URL de QWC Project publisher service n&apos;est pas définie</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="154"/>
+        <source>Project unsaved</source>
+        <translation>Projet non sauvegardé</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="154"/>
+        <source>Continue without saving current project?</source>
+        <translation>Continuer sans enregistrer le projet actuel?</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="215"/>
+        <source>Unable to list projects : %s</source>
+        <translation>Impossible de lister les projets : %s</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="252"/>
+        <source>Unable to login in to qwc auth service : %s</source>
+        <translation>Impossible de s&apos;authentifier dans QWC Auth service</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="345"/>
+        <source>Project download to %s.</source>
+        <translation>Projet téléchargé dans %s.</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="355"/>
+        <source>No project selected</source>
+        <translation>Aucun projet selectionné</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="377"/>
+        <source>Project already exists</source>
+        <translation>Le projet existe déjà</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="377"/>
+        <source>Replace online project?</source>
+        <translation>Remplacer le projet en ligne?</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="389"/>
+        <source>Current project is not saved.</source>
+        <translation>Projet actuel non sauvegardé.</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="394"/>
+        <source>Publishing ...</source>
+        <translation>Publication ...</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="402"/>
+        <source>Unable to publish project %s : %s</source>
+        <translation>Impossible de publier le projet %s : %s</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="404"/>
+        <source>Project %s published</source>
+        <translation>Projet %s publié</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="416"/>
+        <source>Delete project</source>
+        <translation>Suppresion du projet</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="416"/>
+        <source>Are you sure to delete permanently %s project in QWC?</source>
+        <translation>Etes-vous sûr(e) de supprimer le projet %s dans QWC de manière permanente?</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="431"/>
+        <source>Unable to delete project %s : %s</source>
+        <translation>Impossible de supprimer le projet %s : %s</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget.py" line="433"/>
+        <source>Project %s deleted</source>
+        <translation>Projet %s supprimé</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectPublisherDockWidgetBase</name>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="14"/>
+        <source>Project Publisher for QWC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="36"/>
+        <source>Connect</source>
+        <translation>Se connecter</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="21"/>
+        <source>Use AuthID</source>
+        <translation>Utiliser un AuthId</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="45"/>
+        <source>QWC Project Publisher service :</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="52"/>
+        <source>http://127.0.0.1:5100</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="113"/>
+        <source>Projects in QWC</source>
+        <translation>Projets dans QWC</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="124"/>
+        <source>Load</source>
+        <translation>Charger</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="131"/>
+        <source>Delete</source>
+        <translation>Supprimer</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="79"/>
+        <source>Use local filename</source>
+        <translation>Nom de fichier actuel</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="89"/>
+        <source>Use QWC filename</source>
+        <translation>Nom de fichier dans QWC</translation>
+    </message>
+    <message>
+        <location filename="../project_publisher_dockwidget_base.ui" line="104"/>
+        <source>Publish</source>
+        <translation>Publier</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/project_publisher.pro
@@ -0,0 +1,3 @@
+FORMS = ../project_publisher_dockwidget_base.ui
+SOURCES = ../project_publisher.py ../project_publisher_dockwidget.py
+TRANSLATIONS = ProjectPublisher_fr.ts
